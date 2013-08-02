55bf9a8d6ba27994ec40bf62dd0fb2faa57fb394
Steven Zhang <shsteven1000@gmail.com>
2013-08-02 10:41:55 +0800

    Initial Commit

---
+++ ZSSemanticsActionTransformerDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ZSSemanticsActionTransformerDemo
+//
+//  Created by Steve on 2/8/13.
+//  Copyright (c) 2013 MagicalBits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ZSSemanticsActionTransformerDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ZSSemanticsActionTransformerDemo
+//
+//  Created by Steve on 2/8/13.
+//  Copyright (c) 2013 MagicalBits. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
